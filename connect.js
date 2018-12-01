dec4fda9b2a416a02ab6f5a3b9acd98ed05a3a54
Sönmez Kartal <szkl89@gmail.com>
2018-12-01 06:20:41 +0300

    Initial commit

---
+++ connect.js
@@ -0,0 +1,40 @@
+const net = require('net')
+
+const shortid = require('shortid')
+
+const Module = require('@microverse-network/module')
+
+const makeArrayTransformer = require('./arraytransformer')
+const makeBufferTransformer = require('./buffertransformer')
+
+module.exports = class TCPSocketProxyConnect extends Module {
+  static get _name() {
+    return 'tcp-socket-proxy'
+  }
+
+  handleReady() {
+    super.handleReady()
+    this.createServer()
+  }
+
+  createServer() {
+    this.server = new net.Server()
+    this.server.listen(this.options.port || 0, () => {
+      this.emit('listening')
+    })
+    this.server.on('connection', socket => {
+      const stream = this.mux.createStream(shortid.generate())
+      this.bindSocketStream(socket, stream)
+    })
+  }
+
+  handleOutgoingConnection(connection) {
+    super.handleOutgoingConnection(connection)
+    this.mux = connection.mux
+  }
+
+  bindSocketStream(socket, stream) {
+    socket.pipe(makeArrayTransformer()).pipe(stream)
+    stream.pipe(makeBufferTransformer()).pipe(socket)
+  }
+}
